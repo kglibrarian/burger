2a5537aa6967d5309cc0960ff0abc5ffce1e49f0
Karen E Gutzman <keg827@fsm.northwestern.edu>
2018-05-24 17:55:16 -0500

    added heroku jawsdb code

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,23 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
-
+var connection;
 // MySQL DB Connection Information (remember to change this with our specific credentials)
-var connection = mysql.createConnection({
-    port: 3306,
+  //Added code for Heroku Jaws DB
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
     host: "localhost",
     user: "Karen",
     password: "test827",
     database: "burgers_db"
   });
-  
+}; 
+
+
+
+
   // Initiate MySQL Connection.
   connection.connect(function(err) {
     if (err) {
